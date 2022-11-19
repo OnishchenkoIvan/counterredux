5aa7e789b182e886b35ef90fa00459aac74333ce
Ivan Onishchenko <Lightsout07@mail.ru>
2022-11-19 12:44:22 +0300

    create branch develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Setter } from "./components/Setter/Setter";
+import { Counter } from "./components/Counter/Counter";
+
+let maxValue = 5;
+let startValue = 0;
 
 function App() {
+  const [count, setCount] = useState<number>(startValue);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const numberMaxValue = localStorage.getItem("maxValue");
+    const numberStartValue = localStorage.getItem("startValue");
+
+    if (numberMaxValue) {
+      maxValue = Number(numberMaxValue);
+    }
+    if (numberStartValue) {
+      startValue = Number(numberStartValue);
+    }
+  }, [count]);
+
+  const Increment = () => {
+    setCount(count + 1);
+  };
+
+  const Reset = () => {
+    setCount(startValue);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Setter setCount={setCount} setError={setError} />
+      <Counter
+        error={error}
+        countCurrent={count}
+        onClickIncrement={Increment}
+        onClickReset={Reset}
+        countMax={maxValue}
+        countStart={startValue}
+      />
     </div>
   );
 }
